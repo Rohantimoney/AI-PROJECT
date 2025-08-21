0bd5fafa4e6c645f7b55e97fc68700238262fda9
RohanBeri04 <rohan@skinfotechies.in>
2025-08-21 23:17:34 +0530

    in main

--- app/main.py
+++ app/main.py
@@ -63,6 +63,7 @@ vector_store = Chroma.from_documents(
     documents=chunks, 
     embedding=embeddings
 )
+"added some ocomment"
 
 # Create a retriever
 retriever = vector_store.as_retriever(search_kwargs={"k": 3})
